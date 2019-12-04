e550887ab313407cac00e29f473e471b8b12a867
Tello- <lollisjosh@gmail.com>
2019-12-03 19:17:02 -0800

    Continued fleshing out FontPool class

--- Source/Snake/include/FontPool.h
+++ Source/Snake/include/FontPool.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <map>
 #include <string>
+#include <vector>
 #include <SFML/Graphics/Font.hpp>
+
 
 
 class FontPool
@@ -11,9 +13,10 @@ public:
 	void createFont(const std::string& _key, const std::string& _filePath);
 	bool removeFont(const std::string& _key);
 	bool findFont(const std::string& _targetFont) const;
+	const std::vector<std::string> fontKeyList() const;
 	
 	inline bool isEmpty() const { return m_bIsEmpty; }
 private:
-	std::map<std::string, sf::Font> m_fonts;
+	std::map<std::string, sf::Font*> m_fonts;
 	bool m_bIsEmpty;
 };
--- Source/Snake/source/FontPool.cpp
+++ Source/Snake/source/FontPool.cpp
@@ -2,14 +2,43 @@
 
 void FontPool::createFont(const std::string& _key, const std::string& _filePath)
 {
+	auto tempFontPointer = new sf::Font;
+	if (!tempFontPointer->loadFromFile("../../../Assets/Font/Nunito_Sans/NunitoSans-Bold.ttf"))
+	{
+		//std::cerr << "Font failed to load..." << std::endl;
+		/* TODO: Find out what is the best way to handle a case of a font not found.
+		Would this be exception-worthy?*/
+	}
+	else
+	{
+		m_fonts.insert(std::pair<std::string, sf::Font*>{_filePath, tempFontPointer});
+	}
 }
 
 bool FontPool::removeFont(const std::string& _key)
 {
-	return false;
+	if (!findFont(_key))
+	{
+		return false;
+	}
+	else
+	{
+		m_fonts.erase(_key);
+		return true;
+	}
 }
 
 bool FontPool::findFont(const std::string& _targetFont) const
 {
-	return false;
+	auto it = m_fonts.find(_targetFont);
+	return it != m_fonts.end();
+}
+const std::vector<std::string> FontPool::fontKeyList() const
+{
+	std::vector<std::string> keyVector;
+	for (auto it = m_fonts.begin(); it != m_fonts.end(); ++it)
+	{
+		keyVector.push_back(it->first);
+	}
+	return keyVector;
 }
