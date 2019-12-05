07e1d3252dc1d6f7c0d5ca5c7f8a0314c7f5859b
Tello- <lollisjosh@gmail.com>
2019-12-04 16:34:30 -0800

    Hopefully stopped vs from moving files

---
+++ Source/Snake/include/ConfigLoader.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class File
+{
+public:
+	File();
+	void loadFile(const std::string& _fp);
+	inline const std::string& contents() const { return m_fileString; }
+	inline bool isLoaded() const { return m_isLoaded; }
+
+private:
+	std::string m_fileString;
+	bool m_isLoaded;
+};
---
+++ Source/Snake/source/ConfigLoader.cpp
@@ -0,0 +1,22 @@
+#include "../include/ConfigLoader.h"
+#include <fstream>
+#include <iterator>
+#include <cassert>
+
+File::File ()
+	: m_fileString{ "" }, m_isLoaded{ false }
+{}
+
+/* Loads contents of given filepath into File object.
+	Note! Successive loads will overwrite prior contents*/
+void File::loadFile(const std::string& _fp)
+{
+	assert(_fp != "");	
+	std::ifstream l_inFStream{ _fp };
+	std::string l_fileContents{ std::istreambuf_iterator<char>(l_inFStream), std::istreambuf_iterator<char>() };
+	assert(l_fileContents != "");
+
+	m_fileString = l_fileContents;
+	m_isLoaded = true;
+	l_inFStream.close();
+}
