965dc1e597979434b8b75b9bd7ca4882c20497ee
Tello- <lollisjosh@gmail.com>
2019-12-06 10:25:23 -0800

    Added some commenting for clarity

--- Source/Snake/source/FileParser.cpp
+++ Source/Snake/source/FileParser.cpp
@@ -11,6 +11,10 @@
 Config::FileParser::FileParser()
 {}
 
+
+/*	@Param: _assetType : type tag to be assigned to this asset file
+	@Param: _filePath : string path to the file to be parsed
+	Warning: Throws std::runtime_error on bad filepath */
 Config::ParsedFile* Config::FileParser::Parse(const Config::AssetType & _assetType, const std::string & _filePath)
 {
 	/* Open provided file with ifstream */
@@ -20,7 +24,13 @@ Config::ParsedFile* Config::FileParser::Parse(const Config::AssetType & _assetTy
 	{
 		/*rapidxml throws if it fails to read the stream. */
 		/* using heap memory to avoid stack overflow warning */
-		std::unique_ptr<rapidxml::file<>> xmlFile(new rapidxml::file<>(l_inStream));
+		std::unique_ptr<rapidxml::file<>> lp_xmlFile(new rapidxml::file<>(l_inStream));
+		/* l_doc will represent the head of the DOM tree holding the parsed xml info*/
+		rapidxml::xml_document<> l_doc;
+		/* parsing and pointing the l_doc pointer to the DOM head */
+		l_doc.parse<0>(lp_xmlFile->data());
+
+
 	}
 	catch (std::runtime_error _exc)
 	{
@@ -29,7 +39,7 @@ Config::ParsedFile* Config::FileParser::Parse(const Config::AssetType & _assetTy
 	}
 
 
-	rapidxml::xml_document<> l_doc{};
+	
 	l_inStream.close();
 	return nullptr;
 }
