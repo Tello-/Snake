6051e6857b6ad16f216b8331468536cd1a6f2e86
Tello- <lollisjosh@gmail.com>
2019-12-05 20:05:51 -0800

    Nightly commit

--- Source/Snake/source/FileParser.cpp
+++ Source/Snake/source/FileParser.cpp
@@ -1,4 +1,6 @@
 #include "../include/FileParser.h"
+#include <rapidxml.hpp>
+#include <rapidxml_utils.hpp>
 
 
 Config::FileParser::FileParser()
@@ -6,5 +8,5 @@ Config::FileParser::FileParser()
 
 Config::ParsedFile* Config::FileParser::Parse(const Config::AssetType & _assetType, const TextFile & _textFile)
 {	
-	// TODO: run text file through RapidXML code and parse out the data to fill out the ParsedFile struct to return as a pointer.
+	
 }
