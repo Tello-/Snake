fb8084a0c32a9cb884a2a1780e9824e8fdac7bd6
Tello- <lollisjosh@gmail.com>
2019-12-05 17:18:31 -0800

    I wrote a lengthy note at the top of the FileParser.h file.
    I'm currently not sure what route to take with loading and parsing these XML config files. See note in FileParser.h for more info.
    
    There are potentially 3 types of XML config files I will be using. Font, Image, and Sound. These 3 config files are bound to have different
    layouts.. or will they maybe they are all comprised simply of a type, id and file path... This might be a good idea actually. I need to think about it.

--- Source/Snake/include/FileParser.h
+++ Source/Snake/include/FileParser.h
@@ -1,9 +1,25 @@
+/* File parser is what takes the TextFile object and separates the XML nodes into separate pieces of data.
+
+	The thing is.. I'm not quite sure how I want to handle this yet. There are at least a few kinds of XML config file types.
+	
+	Font Config, Image Config, Sound Config... I don't think inheritance is the correct route. My first instinct keeps tell me to 
+	make a general FileParser interface and then to inherit this interface for each of the 3 types of XML files to parse. This makes me 
+	feel like this sends the message that these 3 types of files are interchangable and suitable for polymorphism somewhere... However, why
+	would a single system need a generic FileParser object and then manipulate or deal with one of these derived classes?*/
+
 #pragma once
+#include <string>
+#include "TextFile.h"
 
 namespace Config
 {
+	
 	class FileParser
 	{
+	public:
+		
+	private:
 
 	};
+
 }
--- Source/Snake/source/FileParser.cpp
+++ Source/Snake/source/FileParser.cpp
@@ -1,1 +1,5 @@
 #include "../include/FileParser.h"
+
+
+Config::FileParser::FileParser(const AssetType& _assetType, const TextFile& _file)
+{}
