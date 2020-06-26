483086d168b64fac0c981a03c0d589d413884a95
Tello- <lollisjosh@gmail.com>
2020-06-26 16:29:02 -0700

    init commit, scrapped a lot aof code

--- Source/Snake/include/Game.h
+++ Source/Snake/include/Game.h
@@ -23,39 +23,18 @@ private:
 	void Update();
 	void Render();
 
-	void CheckCollision();
-
-	void DrawShadowText();
-
-	void InitMessageDatabase();
-
 	bool IsHighScore() const;
 	bool m_isHighScore;
-	World m_world;
-	
-	
+	World m_world;	
 
 	sf::RenderWindow m_window;
-	sf::View m_gameView;
-	sf::View m_loseView;
 	
 	sf::Clock m_clock;
 	float m_elapsed;
 	float m_fps;
 	float m_frameTime;
 
-	
-
-	sf::Font m_scoreFont; // make this a std::shared_ptr
-	sf::Text m_scoreText;
-	sf::Text m_highScoreText;
-
 	std::string m_highScoreFilePath;
-
-	sf::Text m_livesText;
-
-	sf::Text m_loseMsg;
-	sf::Text m_pauseMsg;
 
 	MessageDatabase m_messageDatabase;
 
--- Source/Snake/source/Game.cpp
+++ Source/Snake/source/Game.cpp
@@ -9,58 +9,18 @@ Game::Game()
 	//TODO: I Hate everything about this constructor. There is too much going on that is potential for headaches
 	m_window.setKeyRepeatEnabled(false);
 
-	// Load it from a file
-	if (!m_scoreFont.loadFromFile("../../../Assets/Font/Nunito_Sans/NunitoSans-Bold.ttf"))
-	{
-		std::cerr << "Font failed to load..." << std::endl;
-	}
-	else
-	{
-		m_scoreText.setFont(m_scoreFont);
-		m_scoreText.setCharacterSize(30);
-		m_scoreText.setFillColor(sf::Color::White);
-		m_scoreText.setOutlineColor(sf::Color::Black);
-		m_scoreText.setPosition(sf::Vector2f{4.f, WINDOW_Y * .90f});
+	
 
-		m_highScoreText.setFont(m_scoreFont);
-		m_highScoreText.setCharacterSize(30);
-		m_highScoreText.setFillColor(sf::Color::White);
-		m_highScoreText.setOutlineColor(sf::Color::Black);
-		m_highScoreText.setPosition(sf::Vector2f{ WINDOW_X * .40f, WINDOW_Y * .90f });
-
-		m_livesText.setFont(m_scoreFont);
-		m_livesText.setCharacterSize(24);
-		m_livesText.setFillColor(sf::Color::White);
-		m_livesText.setOutlineColor(sf::Color::Black);
-		m_livesText.setPosition(sf::Vector2f(WINDOW_X * .80f, WINDOW_Y * .01f));
-
-		m_loseMsg.setFont(m_scoreFont);
-		m_loseMsg.setCharacterSize(30);
-		m_loseMsg.setFillColor(sf::Color::Red);
-		m_loseMsg.setOutlineColor(sf::Color::Black);
-		m_loseMsg.setPosition(sf::Vector2f(WINDOW_X * .3f, WINDOW_Y * .5f));
-		m_loseMsg.setString("You have lose...");
-
-		m_pauseMsg = m_loseMsg;
-		m_pauseMsg.setString("Paused...");
-	}
-
-
-	m_messageDatabase.createMessage(MessageType::SCORE, m_scoreFont, "Score: ");
-
-
+	// THIS SHOULD NOT BE IN THE CONSTRUCTOR
 	std::ifstream ifs{ m_highScoreFilePath };
 	ifs >> m_highScore;
 	ifs.close();
-
-
-	m_window.setView(m_gameView);	
 	
 }
 
 Game::~Game()
 {
-	
+	// THIS IS SOMETHING THAT THE DESTRUCTOR SHOULD NOT BE DOING!
 	std::ofstream ofs{ m_highScoreFilePath };
 	ofs << m_highScore;
 	ofs.close();
@@ -84,55 +44,7 @@ void Game::Run()
 
 void Game::Input()
 {
-	sf::Event event;
-	while (m_window.pollEvent(event))
-	{
-		if (event.type == event.Closed)
-			m_window.close();
-	}
-
-	switch (m_state)
-	{
-	case PENDING:
-		break;
-	case PLAYING:
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-		{
-			m_world.getSnake()->SetDirection(Direction::Up);
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-		{
-			m_world.getSnake()->SetDirection(Direction::Left);
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-		{
-			m_world.getSnake()->SetDirection(Direction::Down);
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-		{
-			m_world.getSnake()->SetDirection(Direction::Right);
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			m_state = State::PAUSED;
-		}
-		break;
-	case PAUSED:
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			m_state = State::PLAYING;
-		}
-		break;
-	case LOSE:
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
-		{
-			m_world.getSnake()->Lose();
-			m_state = State::PLAYING;
-		}
-		break;
-	default:
-		break;
-	};
+	
 	
 	
 }
@@ -150,14 +62,7 @@ void Game::Update()
 		{
 			m_highScore = true;
 			m_highScore = m_world.getSnake()->GetScore();
-			m_scoreText.setFillColor(sf::Color::Yellow);
 		}
-
-
-		// TODO: this code "knows" too much. Figure out where else this code fits and/or how to break it down into simpler parts
-		m_scoreText.setString("Score: " + std::to_string(m_world.getSnake()->GetScore()));
-		m_highScoreText.setString("High Score: " + std::to_string(m_highScore));
-		m_livesText.setString("Lives: " + std::to_string(m_world.getSnake()->GetLives()));
 
 		if (m_world.getSnake()->HasLost())
 		{
@@ -167,7 +72,6 @@ void Game::Update()
 	case PAUSED:
 		break;
 	case LOSE:
-		m_scoreText.setFillColor(sf::Color::White);
 		break;
 	default:
 		break;
@@ -185,15 +89,11 @@ void Game::Render()
 	case PLAYING:
 		m_world.Render(m_window);  // TODO replace temporary render setup
 		DrawShadowText();			// TODO this is a terrible method
-		m_window.draw(m_scoreText);
-		m_window.draw(m_highScoreText);
-		m_window.draw(m_livesText);
+	
 		break;
 	case PAUSED:
-		m_window.draw(m_pauseMsg);
 		break;
 	case LOSE:
-		m_window.draw(m_loseMsg);
 		break;
 	default:
 		break;
@@ -201,62 +101,6 @@ void Game::Render()
 	m_window.display();
 }
 
-void Game::CheckCollision()
-{
-
-}
-
-
-
-void Game::DrawShadowText()
-{
-	sf::Text shadowText{ m_scoreText };
-	shadowText.setFillColor(sf::Color::Black);
-	shadowText.setPosition(m_scoreText.getPosition().x + 2, m_scoreText.getPosition().y + 3);
-	m_window.draw(shadowText);
-
-	shadowText={ m_highScoreText };
-	shadowText.setFillColor(sf::Color::Black);
-	shadowText.setPosition(m_highScoreText.getPosition().x + 2, m_scoreText.getPosition().y + 3);
-	m_window.draw(shadowText);
-
-	shadowText = { m_livesText };
-	shadowText.setFillColor(sf::Color::Black);
-	shadowText.setPosition(m_livesText.getPosition().x + 2, m_livesText.getPosition().y + 3);
-	m_window.draw(shadowText);
-}
-
-void Game::InitMessageDatabase()  // TODO replace 
-{
-	m_messageDatabase.createMessage(MessageType::SCORE, m_scoreFont, "Score: ", 30);
-	//m_scoreText.setFont(m_scoreFont);
-	//m_scoreText.setCharacterSize(30);
-	//m_scoreText.setFillColor(sf::Color::White);
-	//m_scoreText.setOutlineColor(sf::Color::Black);
-	m_messageDatabase.getMessage(MessageType::SCORE).setPosition(sf::Vector2f{ 4.f, WINDOW_Y * .90f });
-
-	m_highScoreText.setFont(m_scoreFont);
-	m_highScoreText.setCharacterSize(30);
-	m_highScoreText.setFillColor(sf::Color::White);
-	m_highScoreText.setOutlineColor(sf::Color::Black);
-	m_highScoreText.setPosition(sf::Vector2f{ WINDOW_X * .40f, WINDOW_Y * .90f });
-
-	m_livesText.setFont(m_scoreFont);
-	m_livesText.setCharacterSize(24);
-	m_livesText.setFillColor(sf::Color::White);
-	m_livesText.setOutlineColor(sf::Color::Black);
-	m_livesText.setPosition(sf::Vector2f(WINDOW_X * .80f, WINDOW_Y * .01f));
-
-	m_loseMsg.setFont(m_scoreFont);
-	m_loseMsg.setCharacterSize(30);
-	m_loseMsg.setFillColor(sf::Color::Red);
-	m_loseMsg.setOutlineColor(sf::Color::Black);
-	m_loseMsg.setPosition(sf::Vector2f(WINDOW_X * .3f, WINDOW_Y * .5f));
-	m_loseMsg.setString("loser...");
-
-	m_pauseMsg = m_loseMsg;
-	m_pauseMsg.setString("Paused...");
-}
 
 bool Game::IsHighScore() const
 {
