55c0962e78e1b331da4aa3715fda0ef610370fe7
Tello- <lollisjosh@gmail.com>
2020-06-26 19:05:28 -0700

    Fleshing out state classes

--- Source/Snake/include/SnakeStates.hpp
+++ Source/Snake/include/SnakeStates.hpp
@@ -1,11 +1,40 @@
 #pragma once
 #include "IState.hpp"
+#include "IContext.hpp"
 
 namespace SnakeState
 {
 	class PendingState : public IState
 	{
 	public:
+		PendingState(IContext& context) : IState{ context } {}
+		virtual void handleInput() {}
+		virtual void handleUpdate() {}
+		virtual void handleRender() {}
+	};
+
+	class PlayingState: public IState
+	{
+	public:
+		PlayingState(IContext& context) : IState{ context } {}
+		virtual void handleInput() {}
+		virtual void handleUpdate() {}
+		virtual void handleRender() {}
+	};
+
+	class PauseState: public IState
+	{
+	public:
+		PauseState(IContext& context) : IState{ context } {}
+		virtual void handleInput() {}
+		virtual void handleUpdate() {}
+		virtual void handleRender() {}
+	};
+
+	class LoseState : public IState
+	{
+	public:
+		LoseState(IContext& context) : IState{ context } {}
 		virtual void handleInput() {}
 		virtual void handleUpdate() {}
 		virtual void handleRender() {}
